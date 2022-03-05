a44e7ea077a1a51575f77747e5276f6902a9ed00
nef <83908932+not-nef@users.noreply.github.com>
2022-03-05 14:07:48 +0100

    update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
 
 setup(
     name='ntkutils',
-    version='1.0',
+    version='1.3.0',
     license='MIT',
     author="not-nef",
+    description="Utilities for tkinter",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     packages=find_packages('src'),
     package_dir={'': 'src'},
     url='https://github.com/not-nef/ntkutils',
