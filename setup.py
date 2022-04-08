9dfbd701117772a151259b779af9c72e79cc0f75
nef <83908932+not-nef@users.noreply.github.com>
2022-04-08 19:01:25 +0200

    2.1.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='ntkutils',
-    version='2.0.0',
+    version='2.1.0',
     license='MIT',
     author="not-nef",
     description="Utilities for tkinter",
