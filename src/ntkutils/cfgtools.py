9563109110abfb908999150b104050ea65972b4d
nef <83908932+not-nef@users.noreply.github.com>
2022-05-15 15:42:43 +0200

    Add filename option

--- src/ntkutils/cfgtools.py
+++ src/ntkutils/cfgtools.py
@@ -1,24 +1,22 @@
 import json
 import os 
 
-def LoadCFG():
-    with open("cfg.json") as cfg_file:
-        cfg = json.load(cfg_file)
-
+def LoadCFG(filename):
+    cfg_file = open(filename)
+    cfg = json.load(cfg_file)
+    cfg_file.close()
     return cfg
 
-def SaveCFG(cfg):
-    with open("cfg.json", "w") as cfg_file:
-        json.dump(cfg, cfg_file)
+def SaveCFG(cfg, filename="cfg.json"):
+    cfg_file = open(filename, "w+")
+    json.dump(cfg, cfg_file)
+    cfg_file.close()
 
-def init(dconfig):
-    if not os.path.isfile("cfg.json"):
-        with open("cfg.json", "w") as cfg_file:
-            json.dump(
-                dconfig,
-                cfg_file,
-            )
-
-        return LoadCFG()
+def init(dconfig, filename="cfg.json"):
+    if not os.path.isfile(filename):
+        cfg_file = open(filename, "w+")        
+        json.dump(dconfig, cfg_file)
+        cfg_file.close()
+        return LoadCFG(filename=filename)
     else:
-        return LoadCFG()
+        return LoadCFG(filename)
